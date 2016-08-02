82085b7690c93aa93165123598ce4094767533db
Robert <robert.gol.jr@gmail.com>
2016-08-01 23:19:29 -0300

    lab

---
+++ routes/lab.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+});
+
+router.get('/q/*', function(req, res, next) {
+  var path = req.params[0] ? req.params[0] : 'index.html';
+  res.sendfile(path, {root: './lab/Quadraralho/public'});
+});
+
+module.exports = router;
